d52689ca9b35a1ee3a7c372f21b1c1bdc83ffc25
Houman Amini <h.amini@pep.co.ir>
2024-09-23 05:57:27 +0330

    adjust logo and avator

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'صفحه اصلی' },
   { href: '/blog', title: 'بلاگ' },
   { href: '/tags', title: 'تگ ها' },
-  { href: '/projects', title: 'رزومه' },
+  // { href: '/projects', title: 'رزومه' },
   { href: '/about', title: 'درباره من' },
 ]
 
