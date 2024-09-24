c914793d4729a7354d63a079e35a6d3d152c5695
Houman Amini <h.amini@pep.co.ir>
2024-09-25 00:22:36 +0330

    house keeping

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -34,7 +34,9 @@ export default function AuthorLayout({ children, content }: Props) {
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
             <div className="flex space-x-3 pt-6">
-              <SocialIcon kind="mail" href={`mailto:${email}`} />
+              <div className="ml-2">
+                <SocialIcon kind="mail" href={`mailto:${email}`} />
+              </div>
               <SocialIcon kind="github" href={github} />
               <SocialIcon kind="linkedin" href={linkedin} />
               <SocialIcon kind="x" href={twitter} />
